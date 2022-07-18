3b61ff9b0c6f9283164d32d728e38a8cf7052312
Tim Hiller <thiller@itemis.com>
2022-07-18 19:11:47 +0200

    change to testvault

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
  #!/bin/bash
-
-rm -rf /Users/thiller/Documents/Obsidian/UnseenLibrary/.obsidian/plugins/obsidian-meeting-notes 
-mkdir /Users/thiller/Documents/Obsidian/UnseenLibrary/.obsidian/plugins/obsidian-meeting-notes
+ 
+yarn build 
+rm -rf /Users/thiller/Desktop/TestVault/.obsidian/plugins/obsidian-meeting-notes 
+mkdir /Users/thiller/Desktop/TestVault/.obsidian/plugins/obsidian-meeting-notes
 echo "Copying build files to the obisdian plugin directory..."
-cp main.js /Users/thiller/Documents/Obsidian/UnseenLibrary/.obsidian/plugins/obsidian-meeting-notes/main.js
-cp manifest.json /Users/thiller/Documents/Obsidian/UnseenLibrary/.obsidian/plugins/obsidian-meeting-notes/manifest.json
+cp main.js /Users/thiller/Desktop/TestVault/.obsidian/plugins/obsidian-meeting-notes/main.js
+cp manifest.json /Users/thiller/Desktop/TestVault/.obsidian/plugins/obsidian-meeting-notes/manifest.json
 echo "Done copying"
